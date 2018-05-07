bacdba93b75cb341cf95fed970623e22f3d6b49e
Hunkyo Jung <hunkyo.jung@gmail.com>
2018-05-07 21:10:55 +0900

    Complete course

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://hunkyo:GNSry5412@ds113700.mlab.com:13700/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
